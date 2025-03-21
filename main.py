bdda921c80db81da9123c299296685d97eb1e59f
jcheva <56366969+jcheva@users.noreply.github.com>
2025-03-20 23:25:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 import os
 
 app = Flask(__name__)
-print(HARVEST_API_TOKEN) 
+
 # Configuración de Harvest API
 HARVEST_ACCOUNT_ID = "1961945"  # Reemplázalo con el tuyo
 HARVEST_API_TOKEN = os.getenv("HARVEST_API_TOKEN")  # Lo leeremos desde una variable de entorno
@@ -20,10 +20,10 @@ def fetch_harvest_data():
     response = requests.get(url, headers=headers)
     
     if response.status_code == 200:
-    return jsonify(response.json())
-else:
-    print(f"Error {response.status_code}: {response.text}")  # Imprimir el mensaje de error
-    return jsonify({"error": "No se pudo obtener datos"}), response.status_code
+        return jsonify(response.json())
+    else:
+        print(f"Error {response.status_code}: {response.text}")  # Imprimir el mensaje de error
+        return jsonify({"error": "No se pudo obtener datos"}), response.status_code
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
